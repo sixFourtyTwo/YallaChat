a8c5b4b716a8253a6bb609eca31bccee865c0c3b
DaniKanso <93489329+DaniKanso@users.noreply.github.com>
2024-04-11 23:30:30 +0300

    server update

--- server/server.py
+++ server/server.py
@@ -7,17 +7,20 @@ port = input('Port Number: ')
 
 
 def handler(conn, addr):
-    cmnd = conn.recv(1024).decode('utf-8')
-
-    if(cmnd == 'foo'): #implement commands like this
-        pass #do something
+    message = conn.recv(1024).decode('utf-8').split()
+    cmnd = message[0]
+    if(cmnd == 'LOGIN'): 
+        username, password = message[1], message[2]
+        reply = DB.authenticate(username, password)
+        conn.send(reply.encode())
+        
 
 def Server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((socket.gethostbyname(socket.gethostname()), 50))
 
     print('Server is now online.')
-
+    server.listen()
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handler, args=(conn, addr))
