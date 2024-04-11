0b6bb865196ebdebe71f951e99ebb1b92643c452
DaniKanso <93489329+DaniKanso@users.noreply.github.com>
2024-04-12 00:26:33 +0300

    P for Pain

--- server/DB.py
+++ server/DB.py
@@ -8,10 +8,35 @@ def create_accounts(c):
              name TEXT,
              email TEXT,
            username TEXT,
-             password TEXT)
+             password TEXT);
            ''')
 def create_chats(c):
-    pass
+    c.execute('''CREATE TABLE IF NOT EXISTS chats (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    participant1 TEXT,
+    participant2 TEXT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  -- Timestamp column
+    UNIQUE(participant1, participant2)
+);''')
+def create_messages(c):
+    c.execute('''CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sender TEXT,
+    recipient TEXT,
+    content TEXT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    chat_id INTEGER,
+    FOREIGN KEY (chat_id) REFERENCES chats(id)
+);''')
+
+def fetch_chats(cursor):
+        cursor.execute("SELECT id, participant1, participant2 FROM chats")
+        chats = cursor.fetchall()
+        return chats
+def fetch_messages(cursor, chat_id):
+        messages = cursor.execute("SELECT sender, content, timestamp FROM messages WHERE chat_id = ? ORDER BY timestamp", (chat_id,))
+        messages = cursor.fetchall()
+        return messages
     
     
 def register_account(name, email, username, password):
